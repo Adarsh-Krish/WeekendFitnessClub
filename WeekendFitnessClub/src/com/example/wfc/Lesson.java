aac29442786ea2ba3cb4a11e6979041209acb41f
Adarsh Krishna <uadarsh99@gmail.com>
2023-04-23 00:13:08 +0100

    function to view lesson by day and type added

--- WeekendFitnessClub/src/com/example/wfc/Lesson.java
+++ WeekendFitnessClub/src/com/example/wfc/Lesson.java
@@ -8,8 +8,10 @@ public class Lesson {
     private String weekend;
     private int maxCapacity;
 
-    public Lesson(String name, String day, String weekend, int maxCapacity) {
-        this.fitnessType = name;
+    private int availableCapacity;
+
+    public Lesson(String fitnessType, String day, String weekend, int maxCapacity) {
+        this.fitnessType = fitnessType;
         this.day = day;
         this.weekend = weekend;
         this.maxCapacity = maxCapacity;
@@ -26,4 +28,37 @@ public class Lesson {
     public int getMaxCapacity() {
         return maxCapacity;
     }
+
+    public String getWeekend(){
+        return weekend;
+    }
+
+    public void reduceMaxCapacity(){
+        if (maxCapacity>0){
+            this.availableCapacity = maxCapacity-1;
+        } else {
+            System.out.println("Maximum Capacity is empty");
+        }
+    }
+
+    public void increaseMaxCapacity(){
+        if (maxCapacity<5){
+            this.availableCapacity = maxCapacity+1;
+        } else {
+            System.out.println("Maximum Capacity is Full");
+        }
+    }
+
+    public void viewLessonByDay(String selectedDay){
+        if (selectedDay.equalsIgnoreCase(getDay())){
+            System.out.printf("%-10s %-10s %-10s %-10d %-10d \n", day, fitnessType, weekend, maxCapacity, availableCapacity);
+        }
+    }
+
+    public void viewLessonByFitnessType(String selectedFitnessType){
+        if (selectedFitnessType.equalsIgnoreCase(getFitnessType())){
+            System.out.printf("%-10s %-10s %-10s %-10d %-10d \n", fitnessType, day, weekend, maxCapacity, availableCapacity);
+        }
+    }
+
 }
